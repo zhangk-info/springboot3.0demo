5dfeb06086b22676f443fac4be8a08aff95d0adf
zhangkun <13628344382@139.com>
2023-01-06 18:37:57 +0800

    .

--- framework/src/main/java/com/xlj/framework/runner/SilverApplicationRunner.java
+++ framework/src/main/java/com/xlj/framework/runner/SilverApplicationRunner.java
@@ -1,12 +1,17 @@
 package com.xlj.framework.runner;
 
+import com.xlj.common.entity.AjaxResult;
+import com.xlj.common.entity.DataResp;
 import com.xlj.system.service.ISysConfigService;
 import com.xlj.system.service.ISysDictTypeService;
 import jakarta.annotation.PostConstruct;
+import org.springdoc.core.converters.ConverterUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SilverApplicationRunner implements ApplicationRunner {
 
     @Autowired
@@ -19,6 +24,9 @@ public class SilverApplicationRunner implements ApplicationRunner {
      */
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // 增加自定义的返回类型 用以支持springdoc @link ResponseSupportConverter.resolve isResponseTypeWrapper(cls) org.springdoc.core.converters.ConverterUtils
+        ConverterUtils.addResponseWrapperToIgnore(DataResp.class);
+        ConverterUtils.addResponseWrapperToIgnore(AjaxResult.class);
         // 项目启动时，初始化参数到缓存
         sysConfigService.loadingConfigCache();
         // 项目启动时，初始化字典到缓存
