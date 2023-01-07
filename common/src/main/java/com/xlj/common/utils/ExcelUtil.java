89403661678402c19574cdc79737fe313c09b50b
zhangkun <13628344382@139.com>
2023-01-07 14:54:21 +0800

    .

--- common/src/main/java/com/xlj/common/utils/ExcelUtil.java
+++ common/src/main/java/com/xlj/common/utils/ExcelUtil.java
@@ -1,15 +1,26 @@
 package com.xlj.common.utils;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.metadata.data.WriteCellData;
+import com.alibaba.excel.write.handler.CellWriteHandler;
+import com.alibaba.excel.write.handler.context.CellWriteHandlerContext;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import com.xlj.common.exception.ServiceException;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 
 import java.io.IOException;
 import java.lang.reflect.ParameterizedType;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author zhangkun
@@ -25,11 +36,7 @@ public class ExcelUtil {
         Class bean;
         try {
             Object temp = sourceList.get(0);
-            // ParameterizedType参数化类型，即泛型
-            ParameterizedType p = ((ParameterizedType) temp.getClass()
-                    .getGenericSuperclass());
-            //getActualTypeArguments获取参数化类型的数组，泛型可能有多个
-            bean = (Class) p.getActualTypeArguments()[0];
+            bean = temp.getClass();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new ServiceException("获取List泛型的Class失败");
@@ -41,11 +48,51 @@ public class ExcelUtil {
         // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
         fileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8).replaceAll("\\+", "%20");
         response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
-        try {
-            EasyExcel.write(response.getOutputStream(), bean).sheet("模板").doWrite(sourceList);
+
+
+        // 设置单个单元格的样式 当然样式 很多的话 也可以用注解等方式。
+        WriteCellStyle writeCellStyleData = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.
+        writeCellStyleData.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // 背景绿色
+        writeCellStyleData.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+
+        // 写法3
+        // 这里 需要指定写用哪个class去写
+        try (ExcelWriter excelWriter = EasyExcel.write(response.getOutputStream(), bean).build()) {
+            WriteSheet writeSheet = EasyExcel
+                    .writerSheet("模板")
+                    // 自动列宽
+                    .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
+                    // 头部样式
+                    .registerWriteHandler(new CellWriteHandler() {
+                        @Override
+                        public void afterCellDispose(CellWriteHandlerContext context) {
+                            // 当前事件会在 数据设置到poi的cell里面才会回调
+                            // 判断不是头的情况 如果是fill 的情况 这里会==null 所以用not true
+                            if (Objects.nonNull(context.getHead()) && context.getHead()) {
+                                // 第一个单元格
+                                // 只要不是头 一定会有数据 当然fill的情况 可能要context.getCellDataList() ,这个需要看模板，因为一个单元格会有多个 WriteCellData
+                                WriteCellData<?> cellData = context.getFirstCellData();
+                                // 这里需要去cellData 获取样式
+                                // 很重要的一个原因是 WriteCellStyle 和 dataFormatData绑定的 简单的说 比如你加了 DateTimeFormat
+                                // ，已经将writeCellStyle里面的dataFormatData 改了 如果你自己new了一个WriteCellStyle，可能注解的样式就失效了
+                                // 然后 getOrCreateStyle 用于返回一个样式，如果为空，则创建一个后返回
+                                WriteCellStyle writeCellStyle = cellData.getOrCreateStyle();
+                                // 字体
+                                WriteFont writeFont = new WriteFont();
+                                writeFont.setFontHeightInPoints((short)14);
+                                writeFont.setBold(true);
+                                writeCellStyle.setWriteFont(writeFont);
+
+                            }
+                        }
+                    })
+                    .build();
+            //  写出数据
+            excelWriter.write(sourceList, writeSheet);
         } catch (IOException e) {
-            log.error(e.getMessage(), e);
-            throw new ServiceException("文件输出失败");
+            throw new RuntimeException(e);
         }
     }
 }
