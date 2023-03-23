670276f79fb3ee03c25f73b157c11ad34669f792
zhangkun <13628344382@139.com>
2023-03-23 17:06:49 +0800

    .

--- framework/src/main/java/com/xlj/framework/configuration/webmvc/WebMvcConfig.java
+++ framework/src/main/java/com/xlj/framework/configuration/webmvc/WebMvcConfig.java
@@ -1,13 +1,18 @@
 package com.xlj.framework.configuration.webmvc;
 
+import com.xlj.framework.configuration.jackson.ObjectMapperConfig;
 import com.xlj.framework.interceptor.RepeatSubmitInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * 静态资源配置
@@ -15,7 +20,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @author zhangkun
  */
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurationSupport {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 
     @Autowired
@@ -30,27 +35,27 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
      * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。
      * 需要重新指定静态资源
      *
-     * @param registry
+     * @param registry registry
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        /** 配置knife4j 显示文档 */
+        /* 配置knife4j 显示文档 */
         registry.addResourceHandler("doc.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
+
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
-        super.addResourceHandlers(registry);
     }
 
     /**
      * 放开跨域请求
      */
     @Override
-    protected void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
@@ -66,6 +71,20 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(repeatSubmitInterceptor).addPathPatterns("/**");
+    }
+
+    /**
+     * 使用此方法, 以下 spring-boot: jackson时间格式化 配置 将会失效
+     * spring.jackson.time-zone=GMT+8
+     * spring.jackson.date-format=yyyy-MM-dd HH:mm:ss
+     * 原因: 会覆盖 @EnableAutoConfiguration 关于 WebMvcAutoConfiguration 的配置
+     * */
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        // 设置格式化内容
+        converter.setObjectMapper(new ObjectMapperConfig().getSelfObjectMapper());
+        converters.add(0, converter);
     }
 
 }
